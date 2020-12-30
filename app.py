cbb85c36cd66325cdfa8526c07e7e41c3a70b9f2
Mit Kotak <mitak2@illinois.edu>
2020-12-30 13:50:11 -0600

    make it better

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import json
 import requests
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,15 +11,32 @@ URI = 'http://127.0.0.1:5000'
 st.title("Neural Network")
 st.markdown("Input Image")
 
+
+model = tf.keras.models.load_model('model.h5')
+feature_model = tf.keras.models.Model(
+    model.inputs,
+    [layer.output for layer in model.layers]
+)
+
+_, (x_test,_) = tf.keras.datasets.mnist.load_data()
+x_test=x_test/255
+
+
+def get_prediction():
+    index=np.random.choice(x_test.shape[0])
+    image = x_test[index,:,:]
+    image_arr = np.reshape(image,(1,784))
+    return feature_model.predict(image_arr), image
+
+
+    
 if st.button('Get random prediction'):
-    response = requests.post(URI, data={}) 
-    response = json.loads(response.text)
-    preds = response.get('predictions')
-    image = response.get('image')
+    preds, image = get_prediction()
+    final_preds = [p.tolist() for p in preds]
     image = np.reshape(image,(28,28))
     
     st.sidebar.image(image, width=150)
-    for layer, p in enumerate(preds):
+    for layer, p in enumerate(final_preds):
         numbers = np.squeeze(np.array(p))
         plt.figure(figsize=(32,4))
         if layer ==2:
